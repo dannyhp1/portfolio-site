e867e1b04a150528619c7b49fa04aa377d674bde
Danny Pham <dpham760@gmail.com>
2020-06-30 15:55:15 -0700

    Updated gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,28 @@
-const gulp = require('gulp');
-const sass = require('./build/sass');
-const scripts = require('./build/scripts');
-const images = require('./build/images');
-const sync = require('./build/browsersync');
+const { watch, series, parallel, src, dest, gulp } = require('gulp');
+const connect = require('gulp-connect'); // Runs a local webserver
+const open = require('gulp-open'); // Opens a URL in a web browser
 
-[sass, scripts, images, sync].forEach(task => {
-  task(gulp);
-});
+// Launch Chrome web browser
+// https://www.npmjs.com/package/gulp-open
+function openBrowser(done) {
+    var options = {
+    uri: 'http://localhost:8080'
+    };
+    return src('./')
+    .pipe(open(options));
+    done();
+}
 
-gulp.task('build', gulp.series(['sass', 'scripts', 'images', 'jekyll-build']));
+// Gulp plugin to run a webserver (with LiveReload)
+// https://www.npmjs.com/package/gulp-connect
+function server(done) {
+    return connect.server({
+    root: './',
+    port: 8080,
+    debug: true,
+    });
+    done();
+}
+
+// Default Gulp command
+exports.default = series(openBrowser, server);
